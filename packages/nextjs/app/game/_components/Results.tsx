0d8fe273ca34854e39bdb45c7fb2b036e282d7b1
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-08-05 23:58:55 +0500

    feat: add back home button in results page

--- packages/nextjs/app/game/_components/Results.tsx
+++ packages/nextjs/app/game/_components/Results.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 import { Game } from "~~/types/game/game";
 
 const Results = ({ game }: { game: Game }) => {
+  const router = useRouter();
+
   return (
     <div className="p-6">
       <div className="flex flex-col justify-center">
+        <div className="mx-auto mb-5">
+          <button
+            className="btn btn-sm btn-primary"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            Back to Home
+          </button>
+        </div>
         <h1 className="mx-auto text-2xl">Results</h1>
 
         {game?.winners &&
